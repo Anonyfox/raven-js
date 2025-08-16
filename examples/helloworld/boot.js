c2a591d21db717aaa9410cc92a3fcc6124cd7e21
Maximilian Stroh <Fox56@gmx.de>
2025-08-17 00:27:01 +0200

    fun with examples

--- examples/helloworld/boot.js
+++ examples/helloworld/boot.js
@@ -1,9 +1,11 @@
+// import { ClusteredServer } from "@ravenjs/wings/server";
 import { DevServer } from "@ravenjs/wings/server";
 import { router } from "./src/index.js";
 
 const port = 3000;
 
 const server = new DevServer(router);
+// const server = new ClusteredServer(router);
 server.listen(port).catch(console.error);
 
 console.log(`🚀 Hello World server running at http://localhost:${port}`);
--- examples/helloworld/src/index.js
+++ examples/helloworld/src/index.js
@@ -6,3 +6,11 @@ export const router = new Router();
 router.get("/", (ctx) => {
 	ctx.html(Index);
 });
+
+router.get("/russion-roulette", (ctx) => {
+	if (Math.random() < 0.5) {
+		ctx.html(Index);
+	} else {
+		throw new Error("You died");
+	}
+});
