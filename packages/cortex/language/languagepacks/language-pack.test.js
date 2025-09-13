682fff26c4cf9a1c4a86b559835e691725818b79
Maximilian Stroh <Fox56@gmx.de>
2025-09-13 11:15:57 +0200

    chore: remove English and German language packs along with associated tests and type definitions to streamline the language processing module and improve maintainability

--- packages/cortex/language/languagepacks/language-pack.test.js
+++
@@ -1,8 +0,0 @@
-import assert from "node:assert";
-import { describe, it } from "node:test";
-
-describe("language-pack type", () => {
-	it("typedef exists (sanity)", () => {
-		assert.ok(true);
-	});
-});
